c500f34a7abc5909369c1c2ea71287a979597e47
Dalton Pettus <daltpettus@gmail.com>
2025-05-01 13:07:33 -0700

    refactor and wrap routes in startServer function

--- index.js
+++ index.js
@@ -14,57 +14,41 @@ const MongoClient = require('mongodb').MongoClient;
 const dbConnectionString = process.env.DB_STRING;
 const dbName = 'tasks-list';
 
-const connectMongoClientToDB = async() => {
+const startServer = async() => {
     try {
         const client = await MongoClient.connect(dbConnectionString);
-        console.log(`Connected to ${dbName} database.`);
-        return client.db(dbName);
-    } catch(error) {
-        console.error(error);
-    }
-}
-
-const connectToTasksCollection = async() => {
-    try {
-        const db = await connectMongoClientToDB();
+        console.log(`Connected to ${dbName} Database`)
+        const db = client.db(dbName);
         const tasksCollection = db.collection('tasks');
-        return tasksCollection;
-    } catch(error) {
-        console.error(error);
-    }
-}
 
-const getTasks = async() => {
-    const tasksCollection = await connectToTasksCollection();
-    const tasks = await tasksCollection.find().toArray();
-    return tasks;
-}
-
-app.get('/', async(req, res) => {
-    try {
-        const tasks = await getTasks();
-        console.log(tasks);
-        res.render('index.ejs', { tasks: tasks});
-    } catch(error) {
-        console.error(error);
-    }
-})
-
-app.post('/api/tasks', async(req, res) => {
-    const taskName = req.body.taskName;
-    try {
-        const tasksCollection = await connectToTasksCollection();
-        tasksCollection.insertOne({ taskName: taskName, completed: false })
-            .then(() => {
-                console.log('Task added');
+        app.get('/', async(req, res) => {
+            try {
+                const tasks = await tasksCollection.find().toArray();
+                //console.log(tasks);
+                res.render('index.ejs', { tasks: tasks});
+            } catch(error) {
+                console.error(error);
+            }
+        })
+        
+        app.post('/api/tasks', async(req, res) => {
+            const taskName = req.body.taskName;
+            try {
+                const result = await tasksCollection.insertOne({ taskName: taskName, completed: false });
+                console.log(`${taskName} added`);
                 res.redirect('/');
-            })
+            } catch(error) {
+                console.error(error);
+            }
+        })
+        
+        app.listen(PORT, (req, res) => {
+            console.log(`Server listening on port ${PORT}`);
+        })
 
     } catch(error) {
         console.error(error);
     }
-})
+}
 
-app.listen(PORT, (req, res) => {
-    console.log(`Server listening on port ${PORT}`);
-})
+startServer();
